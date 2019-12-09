80bb5efefd87ef0536dec1f41048ee05f888c112
krists.pudzens <krists.pudzens@finkoinvest.com>
2019-12-09 15:07:52 +0200

    r

--- site/js/main.vue.js
+++ site/js/main.vue.js
@@ -3,6 +3,7 @@ var app = new Vue({
     el: '#mainApp',
     data: {
         view: null,
+        language:'lv',
     },
     computed:{
         sectors:function () {
@@ -37,6 +38,21 @@ var app = new Vue({
         onClick:function (e) {
             console.log('document click event '+e.clientX+", "+e.clientY);
             this.$emit('test-event', 1);
+        },
+        selectDetailView:function(id){
+            //1) rotate to focused perspective
+            //2) fade to blurred main images
+            //3) fade in detail view background
+            //4) fade in detail view content
+            //5) scroll in language tags and buttons
+        },
+        selectMainView: function(){
+            //1) fade oiut detail view
+            //2) fade to sharp images in main view
+            //3) move/rotate to normal perspective
+        },
+        switchLanguage:function(language){
+            this.language = language;
         }
     },
     mounted:function () {
