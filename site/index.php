dd51e0cb2b261a776bb4b26d9f302334f9fcfe9a
houseofbits <krists.pudzens@gmail.com>
2019-12-15 23:53:41 +0200

    t

--- site/index.php
+++ site/index.php
@@ -26,7 +26,9 @@
             </div>
         </div>
 
-        <sector v-for="(sector, index) in sectors" :key="index" :sector="sector"></sector>
+        <!--sector v-for="(sector, index) in sectors" :key="index" :sector="sector"></sector-->
+
+        <clipregion v-for="(sector, index) in sectors" :key="index" :sector="sector"></clipregion>        
 
     </div>
 
@@ -63,6 +65,27 @@
 <script type="text/x-template" id="sector-template">
     <div class="sector" :style="{backgroundImage: 'url(' + backgroundImageUrl + ')'}">
         <div v-if="backgroundImageActiveUrl" class="active" :style="{backgroundImage: 'url(' + backgroundImageActiveUrl + ')'}"></div>
+        <!--svg :width="width" :height="height" style="position:absolute;">
+            <polygon v-if="polygonPoints" :points="polygonPoints" class="polygon"
+            v-on:mousedown="onGrab" 
+            v-on:mouseup="onRelease"
+            v-on:mouseover="onGrab" 
+            v-on:mouseleave="onLeave"
+            v-on:touchstart="onGrab" 
+            v-on:touchend="onRelease"            
+            />
+        </svg-->
+    </div>
+</script>
+<script type="text/x-template" id="dev-sector-template">
+    <div class="dev-sector" :style="{backgroundImage: 'url(' + backgroundImageUrl + ')', width:width+'px', height:height+'px'}">
+        <svg :width="width" :height="height" style="position:absolute;">
+            <polygon v-if="polygonPoints" :points="polygonPoints" class="polygon"/>
+        </svg>
+    </div>
+</script>
+<script type="text/x-template" id="clipregion-template">
+    <div class="clip-region">
         <svg :width="width" :height="height" style="position:absolute;">
             <polygon v-if="polygonPoints" :points="polygonPoints" class="polygon"
             v-on:mousedown="onGrab" 
@@ -75,17 +98,12 @@
         </svg>
     </div>
 </script>
-<script type="text/x-template" id="dev-sector-template">
-    <div class="dev-sector" :style="{backgroundImage: 'url(' + backgroundImageUrl + ')', width:width+'px', height:height+'px'}">
-        <svg :width="width" :height="height" style="position:absolute;">
-            <polygon v-if="polygonPoints" :points="polygonPoints" class="polygon"/>
-        </svg>
-    </div>
-</script>
+
 
 <div class="dev-frame"></div>
 
 <script src="js/sector.vue.js"></script>
+<script src="js/clipregion.vue.js"></script>
 <script src="js/devsector.vue.js"></script>
 <script src="js/main.vue.js"></script>
 </body>
